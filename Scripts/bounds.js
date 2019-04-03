9f7eb54805b9199c5fde5d2c0e91e81891935ea2
Alan Cameron Wills <alan@cameronwills.org>
2019-04-03 17:00:37 +0100

    zone polygons

---
+++ Scripts/bounds.js
@@ -0,0 +1,59 @@
+stDogsOuterBounds = [
+    { longitude: -4.73001659829655, latitude: 52.123394448765 },
+    { longitude: -4.69302035404165, latitude: 52.108328286233 },
+    { longitude: -4.67783597312204, latitude: 52.092577021378 },
+    { longitude: -4.68154412087332, latitude: 52.085028506171 },
+    { longitude: -4.67584866056791, latitude: 52.082550068652 },
+    { longitude: -4.66941865553383, latitude: 52.082426177711 },
+    { longitude: -4.66099209037718, latitude: 52.080638399228 },
+    { longitude: -4.65465301995325, latitude: 52.082040447554 },
+    { longitude: -4.64666969538001, latitude: 52.080331588076 },
+    { longitude: -4.65524502844334, latitude: 52.062512426268 },
+    { longitude: -4.66646681329317, latitude: 52.057402674526 },
+    { longitude: -4.70780984170982, latitude: 52.064373164006 },
+    { longitude: -4.72656346975490, latitude: 52.068032316409 },
+    { longitude: -4.74653902507142, latitude: 52.068962084160 },
+    { longitude: -4.75750297248948, latitude: 52.074011823339 },
+    { longitude: -4.76021876457127, latitude: 52.077815249647 },
+    { longitude: -4.77000703357631, latitude: 52.087387964129 },
+    { longitude: -4.73071861648287, latitude: 52.122928530512 }
+];
+
+stDogsInnerBounds = [
+    { latitude: 52.123394442621, longitude: -4.73001656889249 },
+    { latitude: 52.108328280073, longitude: -4.69302032436701 },
+    { latitude: 52.092577015202, longitude: -4.67783594335142 },
+    { latitude: 52.085028499987, longitude: -4.68154409113668 },
+    { latitude: 52.082550062466, longitude: -4.67584863079247 },
+    { latitude: 52.082426171524, longitude: -4.66941862571277 },
+    { latitude: 52.080638393039, longitude: -4.66099206049888 },
+    { latitude: 52.082040441366, longitude: -4.65465299002973 },
+    { latitude: 52.080331581886, longitude: -4.64666966540380 },
+    { latitude: 52.062512420061, longitude: -4.65524499854258 },
+    { latitude: 52.057402668315, longitude: -4.66646678347524 },
+    { latitude: 52.101110288711, longitude: -4.75625458778457 },
+    { latitude: 52.122928524368, longitude: -4.73071858708487 },
+    { latitude: 52.123846843504, longitude: -4.72989815950851 }
+];
+
+cardiganInnerBounds = [
+    { latitude: 52.131045546682, longitude: -4.71609089667203 },
+    { latitude: 52.092577009025, longitude: -4.67783591358080 },
+    { latitude: 52.085028493803, longitude: -4.68154406140004 },
+    { latitude: 52.082550056280, longitude: -4.67584860101703 },
+    { latitude: 52.082426165337, longitude: -4.66941859589173 },
+    { latitude: 52.080638386850, longitude: -4.66099203062058 },
+    { latitude: 52.082040435178, longitude: -4.65465296010621 },
+    { latitude: 52.080331575696, longitude: -4.64666963542758 },
+    { latitude: 52.066467417962, longitude: -4.64533862154215 },
+    { latitude: 52.094100152117, longitude: -4.58755012297725 },
+    { latitude: 52.153503864098, longitude: -4.62184339078521 },
+    { latitude: 52.130676240313, longitude: -4.71650680268371 }
+];
+
+moylgroveBounds = [
+    { latitude: 52.089293417226, longitude: -4.75778866152719 }, 
+    { latitude: 52.067949920200, longitude: -4.73786739114969 }, 
+    { latitude: 52.049924329701, longitude: -4.75113226133059 }, 
+    { latitude: 52.070286530717, longitude: -4.79156710757124 }
+];
